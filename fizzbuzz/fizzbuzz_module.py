d1359637165876da627d3fc1eaeec9a67856f1cb
PeterBernath <peter.bernath.27@gmail.com>
2017-06-26 23:14:15 +0200

    Add files via upload

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,16 @@
 def fizzbuzz(number):
-    return
-
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    return number
 
 def main():
-    return
+    fizzbuzz(3)
+
+main()
 
 if __name__ == '__main__':
     main()
