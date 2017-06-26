e90b16df99e8845738819fe78a2fc4805d3799c6
PeterBernath <peter.bernath.27@gmail.com>
2017-06-26 23:14:46 +0200

    Add files via upload

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,10 +1,16 @@
+
 def listoverlap(list1, list2):
-    return
+    set3 = set(list1) & set(list2)
+    list3 = list(set3)
+    return list3
 
 
 def main():
-    return
+    list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    return listoverlap(list1, list2)
 
+main()
 
 if __name__ == '__main__':
     main()
