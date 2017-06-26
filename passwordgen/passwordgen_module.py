f73d21afb9d42c73e6dce89c0abb57cab8b580c1
PeterBernath <peter.bernath.27@gmail.com>
2017-06-26 23:15:29 +0200

    Add files via upload

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,16 @@
-def passwordgen():
-    return
+import random
+import string
 
+def passwordgen():
+    phase1 = ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
+    phase2 = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
+    phase3 = ''.join(random.choice(string.digits) for _ in range(3))
+    phase4 = ''.join(random.choice('!@#$%^&*') for _ in range(3))
+    password = phase1 + phase2 + phase3 + phase4
+    return password
 
 def main():
     return
-
 
 if __name__ == '__main__':
     main()
